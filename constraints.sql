be4934578f77c5656d963e91067b552654f70a6a
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-03-13 20:47:09 +0300

    Update constraints.sql

--- constraints.sql
+++ constraints.sql
@@ -1,2 +1,17 @@
 USE ParkingSystem;
 ALTER TABLE User DROP CONSTRAINT user_email;
+
+
+SELECT*
+CREATE VIEW AvailableParkingSlots AS
+SELECT 
+    ps.SlotID,
+    ps.SlotNumber,
+    ps.Location,
+    ps.Type
+FROM ParkingSlot ps
+LEFT JOIN Reservation r 
+    ON ps.SlotID = r.SlotID 
+    AND r.EndTime > CURRENT_TIMESTAMP
+    AND r.StartTime <= CURRENT_TIMESTAMP
+WHERE r
